871d3c44f3668d0dacb92d4bb33fa8250d9e22af
JINS-DE <rlawlstjd044@naver.com>
2024-07-06 16:09:21 +0900

    FINAL_TEST

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request, redirect
+import requests
+from bs4 import BeautifulSoup
 from pymongo import MongoClient  
 
 app = Flask(__name__)
@@ -6,34 +8,60 @@ app = Flask(__name__)
 client = MongoClient('mongodb+srv://sparta:jungle@cluster0.gsunejv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')  # MongoDB Altas 사이트의 링크를 가져옵니다
 db = client.dbjungle 
 
+# index
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+# todo리스트 둥록
+@app.route('/todo', methods=['POST'])
+def create_todo():
+    todoName=request.form['todoName']
+    db.todo.insert_one({'todoName':todoName,'done':False})
+    return jsonify({'result':'success','msg':'등록 완료!'})
+# DB 투두리스트 가져오기
+@app.route('/todo', methods=['GET'])
+def read_todo():
+    todo_list=list(db.todo.find({},{'_id':0}))
+    return jsonify({"data":todo_list})
 
-@app.route('/memo', methods=['POST'])
-def post_memo():
-    title = request.form['input_title']
-    content = request.form['input_text']
-    db.memo.insert_one({'title':title,'content':content})
-    return jsonify({'result':'success','msg':'저장완료!'})
+#업데이트 done
+@app.route("/todo/update/done", methods=['POST'])
+def update_done():
+    todoName=request.form['update_todo']
+    done = request.form['done']
 
-@app.route('/memo', methods=['GET'])
-def read_memo():
-    memo_list=list(db.memo.find({},{'_id':0}))
-    return jsonify({"memo_list":memo_list})
+    # 완료 한 애들은 True, 아직 완료 못 한 애들은 False
+    if done == 'true':
+        done=True
+    else:
+        done=False
 
-@app.route('/api/memo/delete', methods=['POST'])
-def delete_memo():
-    db.memo.delete_one({'title':request.form['delete_title']})
-    return jsonify({'result':'success','msg':'삭제완료!'})
+    db.todo.update_one({'todoName':todoName},{'$set':{"done":done}})
+    return jsonify({'result':'success'})
 
-@app.route('/api/memo/update', methods=['POST'])
-def update_momo():
-    old_title = request.form['old_title']
-    new_title = request.form['new_title']
-    new_content = request.form['new_content']
-    db.memo.update_one({'title':old_title},{'$set':{'title':new_title,'content':new_content}})
-    return jsonify({'result':'success','msg':'수정 완료!'})
+# 업데이트 name
+# @app.route("/todo/update/<string:id>", methods=['POST'])
+# def update_name(id):
+#     new_todo = request.form['new_text']
+#     db.todo.update_one({'todoName': id}, {'$set': {"todoName": new_todo}})
+#     return redirect('/')
 
-if __name__ == '__main__':  
-   app.run('0.0.0.0',port='5000',debug=True)
+@app.route("/todo/update/name", methods=['POST'])
+def update_name():
+    old_todo = request.form['old_todo']
+    new_todo = request.form['new_todo']
+    db.todo.update_one({'todoName': old_todo}, {'$set': {"todoName": new_todo}})
+    return jsonify({'result':'success','msg':'할 일 업데이트 완료!'})
+
+    
+
+
+#삭제
+@app.route("/todo/delete", methods=['POST'])
+def delete_todo():
+    todo = request.form['delete_todo']
+    db.todo.delete_one({'todoName':todo})
+    return jsonify({'result':'success','msg':'할 일 삭제 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
